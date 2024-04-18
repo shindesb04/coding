bd3d59f42bcf7415b4371b45fe00ef7691ec538b
Ganesh Suhas Shinde <ganesh.sb.shinde04@gmail.com>
2024-04-18 07:44:23 -0700

    Time: 36 ms (57.77%), Space: 16.5 MB (69.03%) - LeetHub

--- 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.py
+++ 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.py
@@ -7,35 +7,34 @@
 class Solution:
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
         q = deque([root])
-        p = deque([None])
         
         while q:
             size = len(q)
             xfound = False
             yfound = False
-            xpar = None
-            ypar = None
+            
             for i in range(size):
                 element = q.popleft()
-                parent = p.popleft()
                 if x == element.val:
                     xfound = True
-                    xpar = parent
                 if y == element.val:
                     yfound = True
-                    ypar = parent
+
+                if element.left and element.right:
+                    if element.left.val == x and element.right.val == y:
+                        return False 
+                    if element.left.val == y and element.right.val == x:
+                        return False
                 if element.left:
                     q.append(element.left)
-                    p.append(element)
                 if element.right:
                     q.append(element.right)
-                    p.append(element)
             if xfound and yfound:
-                return xpar != ypar
+                return True
             if xfound or yfound:
                 return False
 
-        return True
+        return False
         
 
         
